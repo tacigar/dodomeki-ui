b527118a78c54ee1aac9ba0e1de24e4828df4aa8
tacigar <ig.lhound@gmail.com>
2020-05-10 09:40:42 +0900

    show a component in storybook

---
+++ src/Button/Button.stories.tsx
@@ -0,0 +1,9 @@
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+
+import Button from './Button';
+
+storiesOf('Button', module)
+  .add('Button', () => (
+    <Button />
+  ));
