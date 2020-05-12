9636b632a9bdabebc1399ff27169290e40c094e7
tacigar <ig.lhound@gmail.com>
2020-05-13 00:14:35 +0900

    update Button component

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { ReactNode, MouseEvent, MouseEventHandler, FC } from 'react';
 
-const Button = () => {
+type SizeType = 'sm' | 'md' | 'lg';
+
+type VariantType = 'filled' | 'outlined';
+
+type ColorType = 'primary' | 'secondary' | 'default';
+
+interface ButtonProps {
+  size?: SizeType;
+  href?: string;
+  onClick?: MouseEventHandler<HTMLButtonElement | HTMLAnchorElement>;
+  children?: ReactNode;
+  variant?: VariantType;
+  color?: ColorType;
+  className?: string;
+}
+
+const Button: FC<ButtonProps> = ({
+  size = 'md',
+  href,
+  onClick,
+  children,
+  variant = 'outlined',
+  color = 'default',
+  className = '',
+}) => {
+  const handleClick = (event: MouseEvent<HTMLButtonElement | HTMLAnchorElement>) => {
+    if (onClick) {
+      onClick(event);
+    }
+  };
+
+  if (href) {
+    return (
+      <a onClick={handleClick}>{children}</a>
+    );
+  }
+
   return (
-    <button>Button</button>
+    <button onClick={handleClick}>{children}</button>
   );
 };
 
