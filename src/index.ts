279cc303ba156a5eca28050821448d2892ecaf4d
tacigar <ig.lhound@gmail.com>
2020-05-17 10:31:54 +0900

    Prepare to publish

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { Button } from './button/button';
+
+export { Theme, defaultTheme } from './styles/theme';
+export { ThemeProviderProps, ThemeProvider } from './styles/theme-provider';
+export { ThemeContext } from './styles/theme-context';
+export { useTheme } from './styles/use-theme';
