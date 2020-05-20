012c5a82af412e6431aa4b1cc5a22c08647c8b79
tacigar <ig.lhound@gmail.com>
2020-05-20 22:29:51 +0900

    Update button component design

--- src/button/button.tsx
+++ src/button/button.tsx
@@ -30,17 +30,17 @@ const getSizeStyles = (props: ButtonProps & { theme: Theme }) => {
     case 'sm':
       return css`
         font-size: ${props.theme.text.fontSize.xsmall};
-        padding: 6px 9px;
+        padding: 8px 11px;
       `;
     case 'md':
       return css`
         font-size: ${props.theme.text.fontSize.base};
-        padding: 8px 11px;
+        padding: 10px 13px;
       `;
     case 'lg':
       return css`
         font-size: ${props.theme.text.fontSize.medium};
-        padding: 11px 15px;
+        padding: 14px 18px;
       `;
     default:
       return '';
@@ -72,9 +72,55 @@ const getColorStyles = (props: ButtonProps & { theme: Theme }) => {
       border: none;
       color: ${props.theme.palette.black};
       &:hover {
-        background-color: ${props.theme.palette.grey.light[2]};
+        background-color: ${props.theme.palette.grey.light[1]};
       }
     `;
+  }
+
+  switch (props.variant) {
+    case 'filled': {
+      let borderColor;
+      let backgroundColor;
+      let hoverBackgroundColor;
+      switch (props.color) {
+        case 'primary':
+          borderColor = props.theme.palette.primary[7];
+          backgroundColor = props.theme.palette.primary[5];
+          hoverBackgroundColor = props.theme.palette.primary[7];
+          break;
+        case 'secondary':
+          borderColor = props.theme.palette.secondary[7];
+          backgroundColor = props.theme.palette.secondary[5];
+          hoverBackgroundColor = props.theme.palette.secondary[7];
+          break;
+      }
+      return css`
+        border: 1px solid ${borderColor};
+        color: ${props.theme.palette.white};
+        background-color: ${backgroundColor};
+        &:hover {
+          background-color: ${hoverBackgroundColor};
+        }
+      `;
+    }
+    case 'outlined': {
+      let borderColor;
+      switch (props.color) {
+        case 'primary':
+          borderColor = props.theme.palette.primary[5];
+          break;
+        case 'secondary':
+          borderColor = props.theme.palette.secondary[5];
+          break;
+      }
+      return css`
+        border: 1px solid ${borderColor};
+        color: ${props.theme.palette.black};
+        &:hover {
+          background-color: ${props.theme.palette.grey.light[1]};
+        }
+      `;
+    }
   }
 
   const borderStyles = getBorderStyles(props);
