fec70687d3bdc9539faa2e2a3f0d7a45060e32f7
tacigar <ig.lhound@gmail.com>
2020-05-24 17:02:00 +0900

    Setup eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  extends: [
+    'airbnb-typescript',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
+  rules: {
+    'no-underscore-dangle': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-cycle': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.tsx'] }],
+    'react/jsx-props-no-spreading': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+    'jsx-a11y',
+    'import',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  overrides: [
+    {
+        files: ['**/*.tsx'],
+        rules: {
+          'react/prop-types': 'off',
+        },
+    },
+  ],
+};
