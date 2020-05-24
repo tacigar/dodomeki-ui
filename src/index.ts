c920c854de7a9c1c8103eae237d2190a07b081b0
tacigar <ig.lhound@gmail.com>
2020-05-24 18:58:05 +0900

    Update index.js

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export { Button } from './button/button';
 
 export { Theme, defaultTheme } from './styles/theme';
-export { ThemeProviderProps, ThemeProvider } from './styles/theme-provider';
-export { ThemeContext } from './styles/theme-context';
+export { ThemeProvider } from './styles/theme-provider';
 export { useTheme } from './styles/use-theme';
