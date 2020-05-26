9456425716603eb9b3a6792ba530923d867b81e5
tacigar <ig.lhound@gmail.com>
2020-05-26 23:49:17 +0900

    Update ThemeProvider

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { addDecorator } from '@storybook/react';
 
-import { ThemeProvider } from '../src/styles/theme-provider';
-import { defaultTheme } from '../src/styles/theme';
+import { ThemeProvider } from '../src/styles';
 
-addDecorator(Story => <ThemeProvider theme={defaultTheme}><Story /></ThemeProvider>)
+addDecorator(Story => (
+  <ThemeProvider>
+    <Story />
+  </ThemeProvider>
+));
--- src/styles/theme-provider.tsx
+++ src/styles/theme-provider.tsx
@@ -1,1 +1,12 @@
-export { ThemeProvider } from 'styled-components';
+import React from 'react';
+import { ThemeProvider as SCThemeProvider } from 'styled-components';
+
+import { defaultTheme } from './theme';
+
+export interface ThemeProviderProps {
+  children: React.ReactNode;
+}
+
+export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => (
+  <SCThemeProvider theme={defaultTheme}>{children}</SCThemeProvider>
+);
