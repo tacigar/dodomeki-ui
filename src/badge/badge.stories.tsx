eda11b02a208ec19cb5cb4c05ab9b4bd63a6700a
tacigar <ig.lhound@gmail.com>
2020-05-26 22:25:09 +0900

    Add badge stories

---
+++ src/badge/badge.stories.tsx
@@ -0,0 +1,43 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+import { storiesOf } from '@storybook/react';
+import { Badge } from './badge';
+
+storiesOf('Badge', module).add('Badge', () => (
+  <div>
+    <div css={{ '& > div': { marginRight: 3 } }}>
+      <Badge variant="outlined" color="primary">
+        Primary
+      </Badge>
+      <Badge variant="outlined" color="secondary">
+        Secondary
+      </Badge>
+      <Badge variant="outlined" color="success">
+        Success
+      </Badge>
+      <Badge variant="outlined" color="warning">
+        Warning
+      </Badge>
+      <Badge variant="outlined" color="danger">
+        Danger
+      </Badge>
+    </div>
+    <div css={{ '& > div': { marginRight: 3 } }}>
+      <Badge variant="filled" color="primary">
+        Primary
+      </Badge>
+      <Badge variant="filled" color="secondary">
+        Secondary
+      </Badge>
+      <Badge variant="filled" color="success">
+        Success
+      </Badge>
+      <Badge variant="filled" color="warning">
+        Warning
+      </Badge>
+      <Badge variant="filled" color="danger">
+        Danger
+      </Badge>
+    </div>
+  </div>
+));
