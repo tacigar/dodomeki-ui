00daaac8cf3673486d6ec6aafe84817ff88be971
tacigar <ig.lhound@gmail.com>
2020-05-28 10:11:45 +0900

    Setup backgrounds addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import { addDecorator } from '@storybook/react';
+import { addDecorator, addParameters } from '@storybook/react';
 
 import { ThemeProvider } from '../src/styles';
+
+addParameters({
+  backgrounds: [
+    { name: 'default', value: '#fafafa', default: true },
+    { name: 'paper', value: '#3b5998' },
+  ],
+});
 
 addDecorator(Story => (
   <ThemeProvider>
