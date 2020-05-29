aaf1dc3e2b27d30388f9c3f951d9ff56b91dba10
tacigar <ig.lhound@gmail.com>
2020-05-29 09:48:59 +0900

    Add font-family to global-styles

--- src/global-styles/global-styles.tsx
+++ src/global-styles/global-styles.tsx
@@ -5,5 +5,6 @@ import { Theme } from '../styles';
 export const GlobalStyles = createGlobalStyle<{ theme: Theme }>`
   body: {
     background-color: ${(props) => props.theme.palette.grey.light[0]};
+    font-family: Tahoma, Verdana, Segoe, sans-serif;
   }
 `;
