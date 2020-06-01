f73b53a75fd2e3009ebd31993d00c149f1a13548
tacigar <ig.lhound@gmail.com>
2020-06-01 22:20:59 +0900

    Update ButtonGroup stories

--- src/button-group/button-group.stories.tsx
+++ src/button-group/button-group.stories.tsx
@@ -1,20 +1,69 @@
-import React from 'react';
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
 import { storiesOf } from '@storybook/react';
-import { ButtonGroup } from './button-group';
+
+import {
+  ButtonGroup,
+  ButtonGroupSizeType,
+  ButtonGroupColorType,
+} from './button-group';
 import { Button } from '../button';
-import { Box } from '../box';
+import { Panel } from '../panel';
 
 storiesOf('ButtonGroup', module).add('ButtonGroup', () => (
-  <Box display="flex">
-    <ButtonGroup color="primary" variant="outlined">
-      <Button>Item1</Button>
-      <Button>Item2</Button>
-      <Button>Item3</Button>
-    </ButtonGroup>
-    <ButtonGroup color="primary" variant="filled">
-      <Button>Item1</Button>
-      <Button>Item2</Button>
-      <Button>Item3</Button>
-    </ButtonGroup>
-  </Box>
+  <div
+    css={{
+      '& > div': { padding: 12 },
+      '& > div:not(:last-child)': { marginBottom: 12 },
+    }}
+  >
+    <Panel css={{ '& > div:not(:last-child)': { marginBottom: 6 } }}>
+      {(['sm', 'md', 'lg'] as ButtonGroupSizeType[]).map((size) => (
+        <div
+          css={{
+            display: 'flex',
+            '& > div:not(:last-child)': { marginRight: 6 },
+          }}
+        >
+          <ButtonGroup color="primary" variant="outlined" size={size}>
+            <Button>Item1</Button>
+            <Button>Item2</Button>
+            <Button>Item3</Button>
+          </ButtonGroup>
+          <ButtonGroup color="primary" variant="filled" size={size}>
+            <Button>Item1</Button>
+            <Button>Item2</Button>
+            <Button>Item3</Button>
+          </ButtonGroup>
+        </div>
+      ))}
+    </Panel>
+    <Panel css={{ '& > div:not(:last-child)': { marginBottom: 6 } }}>
+      {([
+        'primary',
+        'secondary',
+        'success',
+        'warning',
+        'danger',
+      ] as ButtonGroupColorType[]).map((color) => (
+        <div
+          css={{
+            display: 'flex',
+            '& > div:not(:last-child)': { marginRight: 6 },
+          }}
+        >
+          <ButtonGroup color={color} variant="outlined">
+            <Button>Item1</Button>
+            <Button>Item2</Button>
+            <Button>Item3</Button>
+          </ButtonGroup>
+          <ButtonGroup color={color} variant="filled">
+            <Button>Item1</Button>
+            <Button>Item2</Button>
+            <Button>Item3</Button>
+          </ButtonGroup>
+        </div>
+      ))}
+    </Panel>
+  </div>
 ));
