3c6fb378e070d1d7891508a264a832e7db4bbb21
tacigar <ig.lhound@gmail.com>
2021-03-01 04:12:15 +0900

    Add Card component

---
+++ src/Card/Card.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { Panel } from '../Panel';
+
+export interface CardProps {
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+export const Card: React.FC<CardProps> = ({ children, className, style }) => {
+  return (
+    <Root className={className} style={style}>
+      {children}
+    </Root>
+  );
+};
+
+const Root = styled(Panel)`
+  display: flex;
+  flex-direction: column;
+`;
