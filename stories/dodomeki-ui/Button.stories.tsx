a459299fdaeb17aa5c8b02756c7874896f894175
tacigar <ig.lhound@gmail.com>
2021-08-25 10:25:09 +0900

    Update storybooks of Buttons

--- stories/dodomeki-ui/Button.stories.tsx
+++ stories/dodomeki-ui/Button.stories.tsx
@@ -1,112 +1,28 @@
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
 import { storiesOf } from '@storybook/react';
-import React, { Fragment } from 'react';
 import styled from 'styled-components';
+import { Button, ButtonProps } from '../../packages/dodomeki-ui/src/Button';
 
-import {
-  Button,
-  ButtonColorType,
-  ButtonVariantType,
-  ButtonSizeType,
-} from '../../packages/dodomeki-ui/src/Button';
+storiesOf('Button', module).add('Button', () => {
+  return (
+    <Root>
+      <div>{renderButton('Default', {})}</div>
+      <div>{renderButton('Disabled', { disabled: true })}</div>
+    </Root>
+  );
+});
 
-storiesOf('Button', module).add('Button', () => (
-  <Root>
-    <Rows>
-      {(['filled', 'outlined', 'empty'] as ButtonVariantType[]).map(
-        (variant) => (
-          <div key={variant}>
-            {([
-              { size: 'sm', text: 'Small' },
-              { size: 'md', text: 'Medium' },
-              { size: 'lg', text: 'Large' },
-            ] as { size: ButtonSizeType; text: string }[]).map((size) => (
-              <Button variant={variant} size={size.size} key={size.size}>
-                {size.text}
-              </Button>
-            ))}
-          </div>
-        ),
-      )}
-    </Rows>
-    <Rows>
-      {(['filled', 'outlined', 'empty'] as ButtonVariantType[]).map(
-        (variant) => (
-          <div key={variant}>
-            {([
-              { color: 'primary', text: 'Primary' },
-              { color: 'secondary', text: 'Secondary' },
-              { color: 'success', text: 'Success' },
-              { color: 'warning', text: 'Success' },
-              { color: 'danger', text: 'Danger' },
-            ] as { color: ButtonColorType; text: string }[]).map((color) => (
-              <Button variant={variant} color={color.color} key={color.color}>
-                {color.text}
-              </Button>
-            ))}
-            <Button variant={variant} disabled>
-              Disabled
-            </Button>
-          </div>
-        ),
-      )}
-    </Rows>
-    <Rows>
-      {(['filled', 'outlined', 'empty'] as ButtonVariantType[]).map(
-        (variant) => (
-          <div key={variant}>
-            {([
-              { size: 'sm', text: 'Small' },
-              { size: 'md', text: 'Medium' },
-              { size: 'lg', text: 'Large' },
-            ] as { size: ButtonSizeType; text: string }[]).map((size) => (
-              <Fragment>
-                <Button variant={variant} size={size.size} key={size.size}>
-                  {size.text}
-                </Button>
-                <Button
-                  variant={variant}
-                  size={size.size}
-                  key={size.size}
-                  isLoading
-                >
-                  {size.text}
-                </Button>
-              </Fragment>
-            ))}
-          </div>
-        ),
-      )}
-    </Rows>
-    <Rows>
-      {(['filled', 'outlined', 'empty'] as ButtonVariantType[]).map(
-        (variant) => (
-          <div key={variant}>
-            <Button
-              variant={variant}
-              icon={<FontAwesomeIcon icon={faCoffee} />}
-            >
-              Coffee
-            </Button>
-          </div>
-        ),
-      )}
-    </Rows>
-  </Root>
-));
+const renderButton = (title: string, props: ButtonProps) => {
+  return (
+    <div>
+      <div>{title}</div>
+      <Button {...props}>Button</Button>
+    </div>
+  );
+};
 
 const Root = styled.div`
-  & > div:not(:last-child) {
+  & > * {
     margin-bottom: 12px;
-  }
-  & button {
-    margin-right: 6px;
-  }
-`;
-
-const Rows = styled.div`
-  & > div:not(:last-child) {
-    margin-bottom: 6px;
   }
 `;
