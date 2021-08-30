2ac12714c5b1ff4fdd26a2619c03a82940c2ce70
tacigar <ig.lhound@gmail.com>
2021-08-30 20:47:07 +0900

    Update index.ts of dodomeki-ui/chart

--- packages/dodomeki-ui-chart/src/index.ts
+++ packages/dodomeki-ui-chart/src/index.ts
@@ -1,1 +1,7 @@
 export * from './BarChart';
+export * from './LineSeries';
+export * from './Plot';
+export * from './PlotContext';
+export * from './PlotData';
+export * from './XAxis';
+export * from './YAxis';
