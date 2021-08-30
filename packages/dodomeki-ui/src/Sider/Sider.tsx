e0f914edd556483576a219bac4006cdffc28f8bf
tacigar <ig.lhound@gmail.com>
2021-08-31 07:35:29 +0900

    Add Sider and SiderContent components

---
+++ packages/dodomeki-ui/src/Sider/Sider.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { SiderContent } from '../SiderContent';
+
+const siderWidth = 300;
+
+export interface SiderProps {}
+
+export const Sider: React.FC<SiderProps> = ({ children }) => {
+  let mainContent: React.ReactElement | null = null;
+  let siderContent: React.ReactElement | null = null;
+
+  React.Children.forEach(children, (child) => {
+    if (React.isValidElement(child)) {
+      if (child.type === SiderContent) {
+        siderContent = child;
+      } else {
+        mainContent = child;
+      }
+    }
+  });
+
+  return (
+    <>
+      <Root>{siderContent}</Root>
+      <Main>{mainContent}</Main>
+    </>
+  );
+};
+
+const Root = styled.div`
+  position: fixed;
+  height: 100vh;
+  width: ${siderWidth}px;
+`;
+
+const Main = styled.main`
+  margin-left: ${siderWidth}px;
+`;
