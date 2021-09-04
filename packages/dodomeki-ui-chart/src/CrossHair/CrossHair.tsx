b85af7546e106932e8e6557bbd315e44e89ad043
tacigar <ig.lhound@gmail.com>
2021-09-05 08:15:07 +0900

    Refactor CrossHair component

--- packages/dodomeki-ui-chart/src/CrossHair/CrossHair.tsx
+++ packages/dodomeki-ui-chart/src/CrossHair/CrossHair.tsx
@@ -8,46 +8,30 @@ export const CrossHair: React.FC<CrossHairProps> = () => {
   const { margin, xScales, yScale, mouseNearestNodePosition } =
     usePlotContext();
 
-  const xLineProps = useMemo(() => {
+  const props = useMemo(() => {
     if (!xScales || !yScale) {
       return undefined;
     }
     if (mouseNearestNodePosition) {
       const x = xScales.linear(mouseNearestNodePosition.x);
-      return {
-        x1: x,
-        x2: x,
-        y1: yScale(yScale.domain()[0]),
-        y2: yScale(yScale.domain()[1]),
-      };
-    }
-    return undefined;
-  }, [mouseNearestNodePosition, xScales, yScale]);
-
-  const yLineProps = useMemo(() => {
-    if (!xScales || !yScale) {
-      return undefined;
-    }
-    if (mouseNearestNodePosition) {
       const y = yScale(mouseNearestNodePosition.y);
       return {
-        x1: xScales.linear(xScales.linear.domain()[0]),
-        x2: xScales.linear(xScales.linear.domain()[1]),
-        y1: y,
-        y2: y,
-      };
-    }
-    return undefined;
-  }, [mouseNearestNodePosition, xScales, yScale]);
-
-  const pointProps = useMemo(() => {
-    if (!xScales || !yScale) {
-      return undefined;
-    }
-    if (mouseNearestNodePosition) {
-      return {
-        cx: xScales.linear(mouseNearestNodePosition.x),
-        cy: yScale(mouseNearestNodePosition.y),
+        xLineProps: {
+          x1: x,
+          x2: x,
+          y1: yScale(yScale.domain()[0]),
+          y2: yScale(yScale.domain()[1]),
+        },
+        yLineProps: {
+          x1: xScales.linear(xScales.linear.domain()[0]),
+          x2: xScales.linear(xScales.linear.domain()[1]),
+          y1: y,
+          y2: y,
+        },
+        pointProps: {
+          cx: x,
+          cy: y,
+        },
       };
     }
     return undefined;
@@ -58,9 +42,19 @@ export const CrossHair: React.FC<CrossHairProps> = () => {
       transform={`translate(${margin.left},${margin.top})`}
       style={{ display: mouseNearestNodePosition ? undefined : 'none' }}
     >
-      <line fill="none" stroke="#cacaca" strokeWidth="0.3px" {...xLineProps} />
-      <line fill="none" stroke="#cacaca" strokeWidth="0.3px" {...yLineProps} />
-      <circle fill="#456789" r="4" {...pointProps} />
+      <line
+        fill="none"
+        stroke="#cacaca"
+        strokeWidth="0.3px"
+        {...props?.xLineProps}
+      />
+      <line
+        fill="none"
+        stroke="#cacaca"
+        strokeWidth="0.3px"
+        {...props?.yLineProps}
+      />
+      <circle fill="#456789" r="4" {...props?.pointProps} />
     </g>
   );
 };
