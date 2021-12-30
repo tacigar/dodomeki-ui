076a46a573ebeb831be31c8e8420852ac6a8798b
tacigar <ig.lhound@gmail.com>
2021-12-30 20:26:45 +0900

    Update .storybook/preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,17 @@
 import { addDecorator } from '@storybook/react';
 import React from 'react';
 
-import { ThemeProvider } from '../packages/dodomeki-ui/src/ThemeProvider';
+import { ThemeProvider as OldThemeProvider } from '../packages/dodomeki-ui/src/ThemeProvider';
+import { defaultTheme, ThemeProvider } from '../packages/dodomeki-ui/src/theme';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
 };
 
 addDecorator((Story) => (
-  <ThemeProvider>
-    <Story />
+  <ThemeProvider theme={defaultTheme}>
+    <OldThemeProvider>
+      <Story />
+    </OldThemeProvider>
   </ThemeProvider>
 ));
