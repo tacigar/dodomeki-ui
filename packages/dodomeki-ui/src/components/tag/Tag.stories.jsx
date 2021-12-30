91a65cc2b340ece2b2293c6192b63c51a173906a
tacigar <ig.lhound@gmail.com>
2021-12-31 08:20:27 +0900

    Add the Tag component

---
+++ packages/dodomeki-ui/src/components/tag/Tag.stories.jsx
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2021 Tacigar
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import React from 'react';
+import { Tag } from '.';
+
+export default {
+  title: 'Display/Tag',
+  component: Tag,
+};
+
+const Template = (args) => <Tag {...args} />;
+
+export const Success = Template.bind({});
+Success.args = {
+  children: 'success',
+  color: 'success',
+};
+
+export const Warning = Template.bind({});
+Warning.args = {
+  children: 'warning',
+  color: 'warning',
+};
+
+export const Danger = Template.bind({});
+Danger.args = {
+  children: 'danger',
+  color: 'danger',
+};
