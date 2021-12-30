6b2a1f4942b168c5d7ea4fd855daeee3def4e4b3
tacigar <ig.lhound@gmail.com>
2021-12-30 20:25:07 +0900

    Add a simple dodomeki-ui icon

---
+++ packages/dodomeki-ui/src/icons/DodomekiUiIcon.tsx
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2021 Tacigar
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import { FC } from 'react';
+
+export const DodomekiUiIcon: FC = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      version="1.1"
+      viewBox="0 0 12.7 12.7"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle
+        cx="6.3994"
+        cy="6.3555"
+        r="5.9172"
+        fill="#f00"
+        strokeWidth=".029374"
+      />
+      <circle
+        cx="4.122"
+        cy="4.3117"
+        r="2.2773"
+        fill="#4d4d4d"
+        strokeWidth=".036176"
+      />
+      <ellipse
+        cx="3.9047"
+        cy="4.1201"
+        rx="1.6707"
+        ry="1.6964"
+        fill="#fff"
+        strokeWidth=".030245"
+      />
+      <circle
+        cx="3.8106"
+        cy="4.0003"
+        r="1.4209"
+        fill="#4d4d4d"
+        strokeWidth=".027491"
+      />
+    </svg>
+  );
+};
