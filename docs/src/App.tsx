9f33c9c41cf29db3304e1c9bd27b6dc0eac220e9
tacigar <ig.lhound@gmail.com>
2022-01-02 08:24:12 +0900

    Use List components in the App

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -7,6 +7,9 @@ import {
   HeaderTitle,
   DodomekiUiIcon,
   Tag,
+  List,
+  ListItem,
+  ListSubheader,
 } from '@dodomeki-ui/core';
 import { css } from '@emotion/react/macro';
 
@@ -31,6 +34,36 @@ function App() {
           </Tag>
         </div>
       </Header>
+      <div
+        css={css`
+          margin-top: 48px;
+          display: flex;
+        `}
+      >
+        <nav
+          css={(theme) => css`
+            width: 240px;
+            height: calc(100vh - 48px);
+            background-color: #fafafa;
+            box-shadow: 0 0 8px gray;
+            & > :not(:last-child) {
+              margin-bottom: ${theme.spacing(1.5)};
+            }
+          `}
+        >
+          <List subheader={<ListSubheader title="Data Display" />}>
+            <ListItem>List</ListItem>
+            <ListItem>Table</ListItem>
+            <ListItem>Tag</ListItem>
+            <ListItem>Tooltip</ListItem>
+          </List>
+          <List subheader={<ListSubheader title="Feedback" />}>
+            <ListItem>Alert</ListItem>
+            <ListItem>Dialog</ListItem>
+            <ListItem>Snackbar</ListItem>
+          </List>
+        </nav>
+      </div>
     </ThemeProvider>
   );
 }
