a6dad175d870df99174fcaf75848d13b58903a65
tacigar <ig.lhound@gmail.com>
2023-03-21 14:08:06 +0900

    Add story of the Button component

--- packages/dodomeki-ui/src/buttons/Button.tsx
+++ packages/dodomeki-ui/src/buttons/Button.tsx
@@ -12,6 +12,21 @@ type ButtonButtonProps = {
   href?: never;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
+const buttonSizeCss = {
+  small: css`
+    padding: 0.15rem 0.3rem;
+    font-size: 0.846rem;
+  `,
+  default: css`
+    padding: 0.3rem 0.6rem;
+    font-size: 1rem;
+  `,
+  large: css`
+    padding: 0.46rem 0.92rem;
+    font-size: 1.154rem;
+  `,
+} as const;
+
 export type ButtonProps = {
   children: ReactNode;
   css?: CSSObject;
@@ -39,6 +54,7 @@ export const Button = forwardRef<
       <a
         css={css`
           ${cssProp}
+          ${buttonSizeCss[size]}
         `}
         href={href}
         ref={buttonRef as Ref<HTMLAnchorElement>}
@@ -65,6 +81,7 @@ export const Button = forwardRef<
     <button
       css={css`
         ${cssProp}
+        ${buttonSizeCss[size]}
       `}
       ref={buttonRef as Ref<HTMLButtonElement>}
       type="button"
